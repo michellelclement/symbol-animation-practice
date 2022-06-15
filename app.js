9dcc2adfe7b7aed2dc92ead8b12135a872622a87
Michelle Clement <michellelclement@gmail.com>
2022-06-15 14:09:41 +0000

    Animate on mouse hover

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 //Speaker waves
+
+//Run speaker animation on mouseenter
+const speaker = document.querySelector(".speaker-svg-container");
 const tl = gsap.timeline({ defaults: { duration: 0.3 } });
+gsap.set("#speaker-group", { transformOrigin: "top left" });
 
-gsap.set("#speaker-group", { transformOrigin: "bottom left" });
+speaker.addEventListener("mouseenter", () => {
+  tl.fromTo(
+    ".wave",
+    { x: 0 },
+    {
+      x: 10,
+      y: -10,
+      scale: 1.25,
+      opacity: 0.5,
+      stagger: { each: 0.05, repeat: 5, yoyo: true }
+    }
+  );
+  gsap.set("#speaker", { transformOrigin: "left 75%" });
+  tl.fromTo(
+    "#speaker",
+    { x: 0 },
+    {
+      scaleY: 1.1,
+      yoyo: true,
+      repeat: 5
+    },
+    "<"
+  );
+});
 
-tl.fromTo(
-  ".wave",
-  { x: 0 },
-  { x: 20, scale: 1.25, opacity: 0.5, stagger: { each: 0.1, repeat: -1, yoyo: true } }
-);
--- bits.js
+++ bits.js
@@ -59,3 +59,36 @@ tl.fromTo(
     "<"
   );
   
+
+
+  // Second version
+
+  //Speaker waves
+const tl = gsap.timeline({ defaults: { duration: 0.3 } });
+
+gsap.set("#speaker-group", { transformOrigin: "top left" });
+
+tl.fromTo(
+  ".wave",
+  { x: 0 },
+  {
+    x: 10,
+    y: -10,
+    scale: 1.25,
+    opacity: 0.5,
+    stagger: { each: 0.05, repeat: -1, yoyo: true }
+  }
+);
+
+//Speaker
+gsap.set("#speaker", { transformOrigin: "left 75%" });
+tl.fromTo(
+  "#speaker",
+  { x: 0 },
+  {
+    scaleY: 1.1,
+    yoyo: true,
+    repeat: -1
+  },
+  "<"
+);
